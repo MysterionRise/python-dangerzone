3596f121527844c93e8c8f8f65a19eda091c75da
Konstantin Perikov <pericov.ko@gmail.com>
2021-02-26 23:46:24 +0000

    add brent

---
+++ src/cycle_detection/brent_algo_detection.py
@@ -0,0 +1,10 @@
+from node import Node, generate_linked_list
+
+
+def detect_cycle(head_node: Node) -> bool:
+    pass
+
+
+if __name__ == "__main__":
+    head = generate_linked_list(100, 0.1)
+    print(detect_cycle(head))
