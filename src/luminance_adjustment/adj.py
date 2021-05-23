0bd47995d4e4274200d6a3eea4a3c829f40e8ee8
Konstantin Perikov <pericov.ko@gmail.com>
2021-05-23 20:30:14 +0100

    add new adjustment tech

--- src/luminance_adjustment/adj.py
+++ src/luminance_adjustment/adj.py
@@ -1,11 +1,11 @@
-import math
-from random import randint
 import glob
+import math
+from pathlib import Path
+from random import random
 from typing import List
-import pandas as pd
 
 import matplotlib.pyplot as plt
-from pathlib import Path
+import pandas as pd
 from PIL import Image, ImageEnhance, ImageStat
 
 
@@ -15,29 +15,49 @@ def replace_white(im_file):
     im = Image.open(im_file)
     width, height = im.size
     pixels = im.load()
-    cnt = 0
-    white_cnt = 0
-    brightness = 140
-
-    darkness_factor = 0.9
+    mean = 119
+    std = 10
+    # cnt = 0
+    # white_cnt = 0
+    # brightness = 140
+    #
+    # darkness_factor = 0.9
     mode = im.mode
     new_img = Image.new(mode, (width, height))
     new_pixels = new_img.load()
     for x in range(width):
         for y in range(height):
             (r, g, b, *a) = pixels[x, y]
+            l = luminocity(r, g, b)
+            if random() > 0.5:
+                if l < mean:
+                    new_pixels[x, y] = (
+                        r + r_component(std),
+                        g + g_component(std),
+                        b + b_component(std),
+                        a[0],
+                    )
+                else:
+                    new_pixels[x, y] = (
+                        r - r_component(std),
+                        g - g_component(std),
+                        b - b_component(std),
+                        a[0],
+                    )
+            else:
+                new_pixels[x, y] = pixels[x, y]
             # if r >= 250 and g >= 250 and b >= 250:
             #     white_cnt += 1
             #     new_pixels[x, y] = (r, g, b, 0)
-            if r > brightness or g > brightness or b > brightness and len(a) == 1 and a[0] != 0:
-                # print("{} {}".format(x, y))
-                cnt += 1
-                new_pixels[x, y] = (int(r * darkness_factor), int(g * darkness_factor), int(b * darkness_factor), a[0])
-            else:
-                new_pixels[x, y] = pixels[x, y]
-    print(white_cnt)
-    stat = ImageStat.Stat(new_img)
-    print("{},{}".format(directory + "/" + file, (stat.mean[0], stat.rms[0])))
+            # if r > brightness or g > brightness or b > brightness and len(a) == 1 and a[0] != 0:
+            #     # print("{} {}".format(x, y))
+            #     cnt += 1
+            #     new_pixels[x, y] = (int(r * darkness_factor), int(g * darkness_factor), int(b * darkness_factor), a[0])
+            # else:
+            #     new_pixels[x, y] = pixels[x, y]
+    # print(white_cnt)
+    # stat = ImageStat.Stat(new_img)
+    # print("{},{}".format(directory + "/" + file, (stat.mean[0], stat.rms[0])))
     Path("targets/" + directory + "/").mkdir(parents=True, exist_ok=True)
     new_img.save("targets/" + directory + "/" + file)
     # new_img.show()
@@ -64,15 +84,32 @@ def luminocity(r: int, g: int, b: int) -> float:
     return (0.21 * r) + (0.72 * g) + (0.07 * b)
 
 
+def r_component(value: int) -> int:
+    # return int(0.21 * value)
+    return value
+
+
+def g_component(value: int) -> int:
+    # return int(0.72 * value)
+    return value
+
+
+def b_component(value: int) -> int:
+    # return int(0.07 * value)
+    return value
+
+
 def distribution_for_files(files: List[str]):
     total_lumi = []
     for f in files:
         total_lumi.extend(distribution_of_luminocity(f))
     df = pd.DataFrame(total_lumi)
     print(df.describe())
-    plt.hist(total_lumi, density=True, bins=30)  # density=False would make counts
-    plt.ylabel('Probability')
-    plt.xlabel('Data')
+    plt.hist(
+        total_lumi, density=True, bins=30
+    )  # density=False would make counts
+    plt.ylabel("Probability")
+    plt.xlabel("Data")
     plt.show()
 
 
@@ -90,12 +127,16 @@ def distribution_of_luminocity(im_file):
 
 
 def brightness(im_file):
-    im = Image.open(im_file).convert('L')
+    im = Image.open(im_file).convert("L")
     stat = ImageStat.Stat(im)
     return stat.mean[0], stat.rms[0]
 
 
-if __name__ == '__main__':
-    files = [f for f in glob.glob("prepared_for_cleaning/*/*.png")]
+if __name__ == "__main__":
+
+    # files = [f for f in glob.glob("prepared_for_cleaning/*/*.png")]
+    for f in glob.glob("prepared_for_cleaning/*/*.png"):
+        replace_white(f)
+    files = [f for f in glob.glob("targets/*/*.png")]
     print(len(files))
     distribution_for_files(files)
