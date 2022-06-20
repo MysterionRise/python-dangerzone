62d7855229ec75069870098d020f9bc85fb05892
Konstantin Perikov <pericov.ko@gmail.com>
2022-06-20 23:14:42 +0100

    add day per day

--- src/encoding-conversion/main.py
+++ src/encoding-conversion/main.py
@@ -1,21 +1,32 @@
 import glob
 
 
-def main():
+def convert_to_utf8():
     # list all files in data folder
     files = glob.glob('data/*.txt')
     for file in files:
         with open(file, 'r', encoding='cp1251') as f:
             # read file
             lines = f.readlines()
-            # print(lines)
-            # convert lines from cp1251 to utf8
-            # lines = [line.encode('utf8').decode('cp1251') for line in lines]
-            # lines = [line.encode('utf-8').decode('cp1251') for line in lines]
             # save file
-            with open(f"data-updated/{file.split('/')[1]}", 'w', encoding='utf-8') as f:
+            with open(f"data-updated/{file.split('/')[1]}", 'w',
+                      encoding='utf-8') as f:
                 f.writelines(lines)
 
 
+def get_all_answers():
+    files = glob.glob("data-per-day/*/*.txt")
+    answers = set()
+    for file in files:
+        with open(file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                if 0 < len(line.strip()) < 25:
+                    answers.add(line.strip().lower())
+    for answer in answers:
+        print(answer)
+    print(len(answers))
+
+
 if __name__ == '__main__':
-    main()
+    get_all_answers()
