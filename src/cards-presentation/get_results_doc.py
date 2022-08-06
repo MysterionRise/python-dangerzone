c9a3dd27614dac3839c83971bee26013751af127
Konstantin Perikov <pericov.ko@gmail.com>
2022-08-06 23:01:53 +0100

    add results processing

--- src/cards-presentation/get_results_doc.py
+++ src/cards-presentation/get_results_doc.py
@@ -2,11 +2,22 @@ import pandas as pd
 from glob import glob
 
 
+def get_answer(ind, answers):
+    return answers[ind].split('_')[1]
+
+
+def get_rt(answers):
+    return int(answers[-1])
+
+
 def main():
     participants = pd.read_csv('data/participants.csv')
     participants_map = dict()
     for _, row in participants.iterrows():
-        participants_map[row['seq']] = row['id']
+        if row['seq'] in participants_map:
+            participants_map[row['seq']].append(row['id'])
+        else:
+            participants_map[row['seq']] = [row['id']]
     print(participants_map)
     template = pd.read_excel('data/template.xlsx')
     answers_per_name = dict()
@@ -21,12 +32,38 @@ def main():
                     answers_per_row[idx] = answers
                 idx += 1
         answers_per_name[file.split('/')[-1].split('.')[0]] = answers_per_row
-    print(answers_per_name)
+    results = pd.DataFrame(
+        columns=['name', 'seq', 'Plod', 'Condtion', 'Answer', 'Corr', 'Time'])
+    ind = 0
+    big_ind = 0
     for _, row in template.iterrows():
-        name = participants_map[row['seq']]
-        row['name'] = name
-        # row.to_csv(f'data/{name}.csv', index=False)
-    template.to_csv('data/template.csv', index=False)
+        list_by_seq = participants_map[row['seq']]
+        # check if list_by_seq is list
+        if type(list_by_seq) is list:
+            name = list_by_seq[0]
+        else:
+            name = list_by_seq
+        if name not in answers_per_name:
+            print(name)
+        else:
+            answers = answers_per_name[name].get(big_ind)
+            s = pd.Series(
+                [name, row['seq'], row['Plod'], row['Condtion'],
+                 get_answer(ind, answers), row['Corr'],
+                 get_rt(answers)],
+                index=['name', 'seq', 'Plod', 'Condtion', 'Answer', 'Corr',
+                       'Time'])
+            results = results.append(s, ignore_index=True)
+            ind += 1
+            if ind == 7:
+                ind = 0
+                big_ind += 1
+            if big_ind == 8:
+                # delete name from participants_map
+                del participants_map[row['seq']][0]
+                big_ind = 0
+
+    results.to_csv('data/results.csv', index=False)
 
 
 if __name__ == '__main__':
